27eba2369a29807b34c3201c208a30ca182b0a05
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-09-26 00:34:24 +0200

    v-for

--- app.js
+++ app.js
@@ -2,24 +2,29 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: "I am the Vue lord",
+            /* title: "I am the Vue lord",
             author: 'Pathum',
             age: 33,
             x: 0,
-            y: 0
+            y: 0 */
+            books: [
+                { title: 'Gamperaliya', author: 'Martin wikramasingha' },
+                { title: 'Ape gama', author: 'Martin wikramasingha' },
+                { title: 'Amma', author: 'Sannasgala' },
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e) {
+        /* handleEvent(e) {
             console.log(e.type)
         },
         handleMouseMove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
-        }
+        } */
     }
 })
 
