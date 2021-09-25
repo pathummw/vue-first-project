15318a74c3429a5864470ffa1892e1eacae470ca
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-09-26 01:45:32 +0200

    Filter books

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://pathum.surge.sh/',
             showBooks: true,
             /* title: "I am the Vue lord",
             author: 'Pathum',
@@ -8,9 +9,9 @@ const app = Vue.createApp({
             x: 0,
             y: 0 */
             books: [
-                { title: 'Gamperaliya', author: 'Martin wikramasingha' },
-                { title: 'Ape gama', author: 'Martin wikramasingha' },
-                { title: 'Amma', author: 'Sannasgala' },
+                { title: 'Gamperaliya', author: 'Martin wikramasingha', img: 'assets/1.jpg', isFav: true },
+                { title: 'Ape gama', author: 'Martin wikramasingha', img: 'assets/2.jpg', isFav: false },
+                { title: 'Amma', author: 'Sannasgala', img: 'assets/3.jpg', isFav: true },
             ]
         }
     },
@@ -25,6 +26,14 @@ const app = Vue.createApp({
             this.x = e.offsetX
             this.y = e.offsetY
         } */
+        toggleClick(book) {
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
